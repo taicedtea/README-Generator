3fd047c226a2e2db9b1976266148855b371ea176
taicedtea <54014240+taicedtea@users.noreply.github.com>
2023-01-11 20:20:07 -0500

    Updated README, basic functionality works, user input works

---
+++ Develop/READMEnew.md
@@ -0,0 +1,29 @@
+# Readme Generator
+## Description
+to generate README files for developers
+
+## Table of Contents
+1. [Installation](#installation)
+2. [Usage](#usage)
+3. [License](#license)  
+4. [Contributing](#contributing)  
+5. [Test](#test)  
+6. [Questions?](#questions)
+
+## Installation 
+First you must have node and npm installed on your machine. Next, clode my github repository
+
+## Usage 
+Once you have the neccesary dependencies installed, within your command line navigate to where 'index.js' from your cloned repo is located.
+
+## License 
+This project is under the following license: MIT
+
+## Contributing 
+Anyone can submit pull request.
+
+## Test 
+Same intrsuctions as how to use.
+
+## Questions? 
+Leave a comment on github
--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,90 @@
-// TODO: Include packages needed for this application
+// Required packages for application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// README.md Formatting
+const generateREADME = ({ name, description, installation, usage, license, contributing, test, email }) =>
+`# ${name}
+## Description
+${description}
+
+## Table of Contents
+1. [Installation](#installation)
+2. [Usage](#usage)
+3. [License](#license)  
+4. [Contributing](#contributing)  
+5. [Test](#test)  
+6. [Questions?](#questions)
+
+## Installation 
+${installation}
+
+## Usage 
+${usage}
+
+## License 
+This project is under the following license: ${license}
+
+## Contributing 
+${contributing}
+
+## Test 
+${test}
+
+## Questions? 
+Feel free to email me at: ${email}`;
 
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is the name of your project?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Give your project a short description.',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'How do you install this project?',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'How do you use this project?',
+    },
+    {
+      type: 'input',
+      name: 'contributing',
+      message: 'Who can contribute to this project?',
+    },
+    {
+      type: 'input',
+      name: 'test',
+      message: 'How to test this app',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'How can someone send you questions about this project?',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      choices: ['MIT', 'test1', 'test2', 'none'],
+    },
+  ])
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+  // Creates README with user given information
+  .then((data) => {
+    const readmePageContent = generateREADME(data);
 
-// TODO: Create a function to initialize app
-function init() {}
+    fs.writeFile('READMEnew.md', readmePageContent, (err) =>
+      err ? console.log(err) : console.log('Successfully created README.md!')
+    );
+  });
 
-// Function call to initialize app
-init();
